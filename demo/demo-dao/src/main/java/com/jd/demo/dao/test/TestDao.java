aa5afaca9b6b96b2779f7a9457488aa78ee48713
ningxw <spnxw1989@gmail.com>
2014-08-17 11:16:20 +0800

    Revert "upload the demo"
    This reverts commit 7c8374b1159b5ca18753a73d78f51a635bdfcfff.

--- demo/demo-dao/src/main/java/com/jd/demo/dao/test/TestDao.java
+++
@@ -1,9 +0,0 @@
-package com.jd.demo.dao.test;
-
-import java.util.List;
-
-import com.jd.demo.domain.Test;
-
-public interface TestDao {
-	public List<Test> excuteTest(Test test);
-}
--- demo/demo-service/src/main/java/com/jd/demo/service/test/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.jd.demo.service.test;
-
-import java.util.List;
-
-import com.jd.demo.domain.Test;
-
-public interface TestService {
-	public List<Test> myTest(Test test);
-}
